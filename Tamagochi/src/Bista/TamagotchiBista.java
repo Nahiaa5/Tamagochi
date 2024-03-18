44ea0cc3799450842bf304b43c6487ed1c3cbe73
Nahiaa5 <nahiav2015@gmail.com>
2024-03-18 18:03:29 +0100

    Bista klaseak

--- Tamagochi/src/Bista/TamagotchiBista.java
+++ Tamagochi/src/Bista/TamagotchiBista.java
@@ -1,5 +1,16 @@
 package Bista;
 
-public class TamagotchiBista {
+import javax.swing.JPanel;
+
+public class TamagotchiBista extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public TamagotchiBista() {
+
+	}
 
 }
